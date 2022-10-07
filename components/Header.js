4a8b60de8ebd7eb48df19f9622a6918f950a0f87
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-10-07 23:00:20 +0500

    header update

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,38 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { navLinks } from './_child/NavLinks';
 
 export default function Header() {
 
     const styles = {
         header: 'w-full fixed top-0 left-0 bg-[transparent] z-[100]',
-        nav: 'h-[3rem] flex justify-between place-items-center md:h-[calc(3rem + 1.5rem)] align-center',
+        nav: 'h-[3rem] flex justify-between place-items-center md:h-[calc(3rem + 1.5rem)]',
         nav__logo: 'flex w-[1.5rem] md:w-[2rem]',
-        nav__list: 'flex flex-col align-center gap-y-[1rem] md:flex-row md:gap-x-[3.5rem]',
+        nav__list: 'flex flex-col gap-y-[1rem] md:flex-row md:gap-x-[3.5rem] place-items-center',
         nav__item: '',
         nav__link: 'nav__link text-h2FontSize uppercase font-[600] bg-gradient-to-b from-[#3b3e40] to-[#141515] text-[transparent] bg-clip-text hover:bg-whiteColor hover:text-whiteColor hover:bg-clip-text md:normal-case text-normalFontSize',
+        active__link: 'nav__link text-h2FontSize uppercase font-[600] bg-gradient-to-b from-[#3b3e40] to-[#141515] text-whiteColor bg-clip-text hover:bg-whiteColor hover:text-whiteColor hover:bg-clip-text md:normal-case text-normalFontSize',
         nav__close: 'absolute text-[1.5rem] top-[1rem] right-[1rem] text-whiteColor cursor-pointer md:hidden',
-        nav__toggle: 'text-[1.2rem] text-whiteColor md:hidden',
+        nav__toggle: 'text-[1.2rem] text-whiteColor md:hidden cursor-pointer',
         show__menu: 'top-0',
-        active__link: 'text-whiteColor bg-clip-text',
     }
 
-    const router = useRouter();
+    const [activeLink, setActiveLink] = useState('#home');
+
+    const [navMenuActive, setNavMenuActive] = useState(null);
+
+    const handleClick = (href) => {
+        setActiveLink(href);
+    }
+
+    const navOpen = () => {
+        setNavMenuActive('show');
+    }
+
+    const navClose = () => {
+        setNavMenuActive(null);
+    }
 
     return (
         <header className={styles.header} id="header">
@@ -26,26 +40,24 @@ export default function Header() {
                 <a href="#" className={styles.nav__logo}>
                     <img src="https://i.ibb.co/kDTwh5q/logo.png" alt="" />
                 </a>
-                <div className="nav__menu" id="nav-menu">
+                <div className={navMenuActive ? 'nav__menu__show' : 'nav__menu'} id="nav-menu">
                     <ul className={styles.nav__list}>
                         {
                             navLinks.map((link, index) => {
                                 return (
                                     <li className={styles.nav__item} key={index}>
-                                        <Link href={link.path}>
-                                            <a className={styles.nav__link + " " + (router?.asPath?.split('/')[1] == link.path || "" ? styles.active__link : '').toString()}>{link.name}</a>
-                                        </Link>
+                                        <a className={activeLink == link.path ? styles.active__link : styles.nav__link} onClick={() => handleClick(link.path)}>{link.name}</a>
                                     </li>
                                 );
                             })
                         }
                     </ul>
                     <div className={styles.nav__close} id="nav-close">
-                        <i className="ri-close-line"></i>
+                        <i className="ri-close-line" onClick={navClose}></i>
                     </div>
                 </div>
                 <div className={styles.nav__toggle} id="nav-toggle">
-                    <i className="ri-function-line"></i>
+                    <i className="ri-function-line" onClick={navOpen}></i>
                 </div>
             </nav>
         </header>
